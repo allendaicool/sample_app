4a1b750c88174a2706ede1ff36c6497a32f16800
Your Name <your.email@example.com>
2014-02-23 00:36:09 -0600

    improve

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -50,14 +50,14 @@ describe "Static pages" do
 
   describe "Contact page" do
 
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
-    end
+      it "should have the content 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_content('Contact')
+      end
 
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
+      it "should have the title 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      end
     end
   end
-end
