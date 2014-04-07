7898527ff8c127869e034cf09bfadfbfc2c927e5
Your Name <your.email@example.com>
2014-04-07 01:23:29 -0500

    improve

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,31 @@
 class UsersController < ApplicationController
+  
+  before_action :signed_in_user, only: [:index, :edit, :update]
+    before_action :correct_user,   only: [:edit, :update]
   def show
       @user = User.find(params[:id])
     end
     
+    def index
+      @users = User.all
+     end
   def new
     @user = User.new
   end
   
+  def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "Profile updated"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end 
+  
+  def edit 
+    @user = User.find(params[:id])
+  end
   
   def create
       @user = User.new(user_params)
@@ -25,4 +44,17 @@ class UsersController < ApplicationController
         params.require(:user).permit(:name, :email, :password,
                                      :password_confirmation)
       end
+      
+      def signed_in_user
+        unless signed_in?
+            store_location
+            redirect_to signin_url, notice: "Please sign in." 
+          end
+        end
+          
+      def correct_user
+                @user = User.find(params[:id])
+                redirect_to(root_url) unless current_user?(@user)
+      end
+      
   end
