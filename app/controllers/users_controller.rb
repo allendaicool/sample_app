0cba1da43752f431a7cbf0a8e76882224a9fef06
Your Name <your.email@example.com>
2014-04-07 13:30:24 -0500

    finish user all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   
   before_action :signed_in_user, only: [:index, :edit, :update]
     before_action :correct_user,   only: [:edit, :update]
+    before_action :admin_user,     only: :destroy
+    
   def show
       @user = User.find(params[:id])
     end
@@ -13,6 +15,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def destroy
+      User.find(params[:id]).destroy
+      flash[:success] = "User deleted."
+      redirect_to users_url
+    end
+    
   def update
       @user = User.find(params[:id])
       if @user.update_attributes(user_params)
@@ -56,5 +64,8 @@ class UsersController < ApplicationController
                 @user = User.find(params[:id])
                 redirect_to(root_url) unless current_user?(@user)
       end
+      def admin_user
+            redirect_to(root_url) unless current_user.admin?
+          end
       
   end
