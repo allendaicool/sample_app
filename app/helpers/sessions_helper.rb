6a5dfa26161aa40669d334dfa8ecd4e732d2a56c
Your Name <your.email@example.com>
2014-04-07 02:17:37 -0500

    improve

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,15 +10,6 @@ module SessionsHelper
       !current_user.nil?
     end
   
-    def redirect_back_or(default)
-        redirect_to(session[:return_to] || default)
-        session.delete(:return_to)
-      end
-
-      def store_location
-        session[:return_to] = request.url if request.get?
-      end
-  
   def current_user=(user)
       @current_user = user
     end
@@ -38,4 +29,14 @@ module SessionsHelper
           cookies.delete(:remember_token)
           self.current_user = nil
         end
+        def redirect_back_or(default)
+            redirect_to(session[:return_to] || default)
+            session.delete(:return_to)
+          end
+
+          def store_location
+            session[:return_to] = request.url if request.get?
+          end
+        
+        
 end
