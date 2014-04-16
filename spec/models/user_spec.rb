9bf847a4b6b6eb7a980754052b12b77a6ca1466b
Your Name <your.email@example.com>
2014-04-16 17:30:36 -0500

    bundle exec rspec spec/

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,32 @@ describe User do
     end
    # it { should_not be_valid }
   
-    
+   describe "micropost associations" do
+
+       before { @user.save }
+       let!(:older_micropost) do
+         FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+       end
+       let!(:newer_micropost) do
+         FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+       end
+
+       it "should destroy associated microposts" do
+             microposts = @user.microposts.to_a
+             @user.destroy
+             expect(microposts).not_to be_empty
+             microposts.each do |micropost|
+               expect(Micropost.where(id: micropost.id)).to be_empty
+             end
+           end
+
+
+
+
+       it "should have the right microposts in the right order" do
+         expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+       end
+     end
 
     subject { @user }
 
